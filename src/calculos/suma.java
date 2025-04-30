03e3274f1b918f2557832e721d32b54cec0f6ea2
BELEN <BELEN@DESKTOP-GV24251>
2025-04-30 12:06:05 +0200

    modificado

---
+++ src/calculos/suma.java
@@ -0,0 +1,17 @@
+package calculos;
+
+import java.util.Scanner;
+
+public class suma {
+
+	public static void main (String [] args) {
+		
+		int a=8;
+		int b=5;
+		int suma=0;
+		
+		suma=a+b;
+		System.out.println(suma);
+	
+	}//fin del main
+}//fin de la clase
